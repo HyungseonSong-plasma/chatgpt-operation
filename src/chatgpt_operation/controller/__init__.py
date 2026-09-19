1ce88203259274b848ed3c7603d030a28aec77ff
raven <hssongplasma@gmail.com>
2026-09-19 23:08:52 +0100

    Export controller lifecycle self-test

--- src/chatgpt_operation/controller/__init__.py
+++ src/chatgpt_operation/controller/__init__.py
@@ -1,5 +1,5 @@
 """Controller lifecycle guards."""
 
-from .lifecycle import LifecycleError, evaluate
+from .lifecycle import LifecycleError, evaluate, self_test
 
-__all__ = ["LifecycleError", "evaluate"]
+__all__ = ["LifecycleError", "evaluate", "self_test"]
