bbe478ed8a7914a42eac5cb595745ac26ee50efd
raven <hssongplasma@gmail.com>
2026-09-20 10:49:43 +0100

    Export controller throughput planner

--- src/chatgpt_operation/controller/__init__.py
+++ src/chatgpt_operation/controller/__init__.py
@@ -1,5 +1,13 @@
-"""Controller lifecycle guards."""
+"""Controller lifecycle and throughput guards."""
 
 from .lifecycle import LifecycleError, evaluate, self_test
+from .throughput import ThroughputError, evaluate as evaluate_throughput, self_test as throughput_self_test
 
-__all__ = ["LifecycleError", "evaluate", "self_test"]
+__all__ = [
+    "LifecycleError",
+    "ThroughputError",
+    "evaluate",
+    "evaluate_throughput",
+    "self_test",
+    "throughput_self_test",
+]
