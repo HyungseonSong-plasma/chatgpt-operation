cf3c19b86667c7c617320b8ee468302eb47688e5
raven <hssongplasma@gmail.com>
2026-09-20 21:21:38 +0100

    Export state refresh controller API

--- src/chatgpt_operation/controller/__init__.py
+++ src/chatgpt_operation/controller/__init__.py
@@ -1,13 +1,17 @@
-"""Controller lifecycle and throughput guards."""
+"""Controller lifecycle, throughput, and state-refresh guards."""
 
 from .lifecycle import LifecycleError, evaluate, self_test
 from .throughput import ThroughputError, evaluate as evaluate_throughput, self_test as throughput_self_test
+from .state_refresh import StateRefreshError, evaluate as evaluate_state_refresh, self_test as state_refresh_self_test
 
 __all__ = [
     "LifecycleError",
     "ThroughputError",
+    "StateRefreshError",
     "evaluate",
     "evaluate_throughput",
+    "evaluate_state_refresh",
     "self_test",
     "throughput_self_test",
+    "state_refresh_self_test",
 ]
